39151e30839f48cba1ba3204a12962dc5d6148a8
Yuzunoha <studysteadystandby@gmail.com>
2019-10-05 19:01:21 +0900

    Auto stash before merge of "master" and "origin/master"
    一応できた

--- main.js
+++ main.js
@@ -1,9 +1,45 @@
 'use strict';
-const main = () => {
-  const p = console.log;
-  const sub = () => {
-    p('hello');
-  };
-  sub();
+const calcByOperation = (left, right, operation) => {
+  left = Number(left);
+  right = Number(right);
+  switch (operation) {
+    case '+':
+      return left + right;
+    case '-':
+      return left - right;
+    case '*':
+      return left * right;
+    case '/':
+      return left / right;
+  }
 };
-main();
+const guardAndGetErrMsg = (numLeft, numRight, operation) => {
+  let errMsg = '';
+  if ('' === numLeft) {
+    errMsg += 'A numLeft is empty<br>';
+  }
+  if ('' === numRight) {
+    errMsg += 'A numRight is empty<br>';
+  }
+  if ('+' !== operation && '-' !== operation && '*' !== operation && '/' !== operation) {
+    errMsg += 'Invalid operation : ' + operation + '<br>';
+  }
+  if ('0' === numRight && '/' === operation) {
+    errMsg += 'Cannot divide by zero<br>';
+  }
+  return errMsg;
+};
+const calc = operation => {
+  const text0 = document.getElementById('text0');
+  const numLeft = document.getElementById('numLeft').value;
+  const numRight = document.getElementById('numRight').value;
+  const errMsg = guardAndGetErrMsg(numLeft, numRight, operation);
+  if ('' !== errMsg) {
+    /* NG */
+    text0.innerHTML = errMsg;
+    return;
+  }
+  /* OK */
+  const num = calcByOperation(numLeft, numRight, operation);
+  text0.innerHTML = num;
+};
