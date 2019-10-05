cff974a47ea66ff4a55dd9891e975061bfb29fe1
Yuzunoha <studysteadystandby@gmail.com>
2019-10-05 16:45:07 +0900

    インデントをスペース2つになるようにvscodeを設定した

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 'use strict';
 const main = () => {
-	const p = console.log;
-	const sub = () => {
-		p('hello');
-	};
-	sub();
+  const p = console.log;
+  const sub = () => {
+    p('hello');
+  };
+  sub();
 };
 main();
